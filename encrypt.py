87778d6988cce5144c5bfed3e4bf5658066303c5
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-09-12 17:05:14 -0300

    decrypt

--- encrypt.py
+++ encrypt.py
@@ -1,38 +1,44 @@
-dictionary ={
-' ':'/',
-'a':'=|',
-'b':'==|',
-'c':'===|',
-'d':'====|',
-'e':'=====|',
-'f':'======|',
-'g':'=======|',
-'h':'========|',
-'i':'=========|',
-'j':'==========|',
-'k':'===========|',
-'l':'============|',
-'m':'=============|',
-'n':'==============|',
-'o':'===============|',
-'p':'================|',
-'q':'=================|',
-'r':'==================|',
-'s':'===================|',
-'t':'====================|',
-'u':'=====================|',
-'v':'======================|',
-'w':'=======================|',
-'x':'========================|',
-'y':'=========================|',
-'z':'==========================|'}
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+dictionary = {}
+count = 0
+
+for i in alphabet:
+    dictionary[i] = '-'*(count+1)+'|'
+    count += 1
+
+# Encrypt
 def encrypt(text, conversion_dict, before=None):
-    # if empty:
     if not text: return text
-    # preliminary transformation:
+
     before = before or str.lower
     t = before(text)
+
     for key, value in conversion_dict.items():
         t = t.replace(key, value)
+
     return t
+
+# Decrypt
+encrypted = encrypt('hello, world!', dictionary)
+
+decrypted = []
+sentence = ''
+
+for i in encrypted:
+    if '-' not in i and i not in alphabet and '|' not in i:
+        sentence += i
+
+    if i == '|':
+        decrypted.append(sentence)
+        sentence = ''
+        continue 
+
+    sentence += i 
+print(encrypted)
+
+for i in decrypted:
+    sentence += alphabet[len(i)-1]
+
+print(decrypted)
+print(sentence)
